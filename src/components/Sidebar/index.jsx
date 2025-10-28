41cc0108eb742afe883af3a847d1d664fff25c30
ulvi05 <aghazadeulvi@gmail.com>
2025-10-28 16:41:06 +0400

    some ui bugs fixed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,9 +2,34 @@ import styles from "./index.module.scss";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useWishlist } from "../../services/context/wishlistContext.jsx";
+import { useAuth } from "../../services/context/authContext.jsx";
+import Swal from "sweetalert2";
 
 const Sidebar = ({ active }) => {
   const { wishlist } = useWishlist();
+  const { auth, logout } = useAuth();
+
+  const handleLogout = () => {
+    Swal.fire({
+      title: "Are you sure Logout?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#e50914",
+      cancelButtonColor: "#333",
+      confirmButtonText: "Yes, logout!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        logout();
+        Swal.fire({
+          title: "Logged Out!",
+          text: "See you later",
+          icon: "success",
+        });
+      }
+    });
+  };
+
   return (
     <div className={`${styles.sidebar} ${active && styles.active}`}>
       <ul className={styles["sidebar-ul"]}>
@@ -28,6 +53,29 @@ const Sidebar = ({ active }) => {
             Wishlist <sup>{wishlist.length}</sup>
           </Link>
         </li>
+
+        {auth && (
+          <li>
+            <button className={styles.logoutBtn} onClick={handleLogout}>
+              Logout
+            </button>
+          </li>
+        )}
+
+        {!auth && (
+          <>
+            <li>
+              <Link className={styles["sidebar-link"]} to={"/login"}>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className={styles["sidebar-link"]} to={"/register"}>
+                Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
